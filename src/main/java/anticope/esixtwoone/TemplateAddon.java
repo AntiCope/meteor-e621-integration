32efc2992d52bd79cf9e7ed4d18bad2abc44d685
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-03-22 21:46:39 +0100

    meteor updater

--- src/main/java/anticope/esixtwoone/TemplateAddon.java
+++ src/main/java/anticope/esixtwoone/TemplateAddon.java
@@ -1,12 +1,16 @@
 package anticope.esixtwoone;
 
 import meteordevelopment.meteorclient.MeteorClient;
+import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.Systems;
 import meteordevelopment.meteorclient.systems.hud.HUD;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.fabricmc.loader.api.FabricLoader;
+
 import java.lang.invoke.MethodHandles;
 
 public class TemplateAddon extends MeteorAddon {
@@ -22,4 +26,26 @@ public class TemplateAddon extends MeteorAddon {
 		HUD hud = Systems.get(HUD.class);
 		hud.elements.add(new ImageHUD(hud));
 	}
+
+	@Override
+    public String getWebsite() {
+        return "https://github.com/AntiCope/meteor-e621-integration";
+    }
+
+    @Override
+    public GithubRepo getRepo() {
+        return new GithubRepo("AntiCope", "meteor-e621-integration");
+    }
+
+    @Override
+    public String getCommit() {
+        String commit = FabricLoader
+            .getInstance()
+            .getModContainer("e621-hud")
+            .get().getMetadata()
+            .getCustomValue("github:sha")
+            .getAsString();
+        return commit.isEmpty() ? null : commit.trim();
+
+    }
 }
